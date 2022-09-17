a42eb623841b5524a2e8f26cf3a692a750d02b8f
kubuseg <123patrze123@gmail.com>
2022-09-17 19:01:41 +0200

    Added Coin Prefab, added lost scene

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    private Collider _collider;
+    private Transform _coinTransform;
+    private CapsuleCollider _capsuleCollider;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _coinTransform = GetComponent<Transform>();
+        _capsuleCollider = GetComponent<CapsuleCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,17 +1,14 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     private Rigidbody _rigidbody;
     private const int JumpForce = 5;
-    private const int SidewayForce = 2;
+    private const int SidewaysForce = 2;
     private bool _jumpPressed;
     private float _horizontalInput;
-    [SerializeField] private Transform _groundCheckTransform;
-    [SerializeField] private LayerMask _playerMask;
+    [SerializeField] private Transform groundCheckTransform;
+    [SerializeField] private LayerMask playerMask;
 
     // Start is called before the first frame update
     void Start()
@@ -29,14 +26,14 @@ public class Player : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if (Physics.OverlapSphere(_groundCheckTransform.position, 0.1f, _playerMask).Length != 0 && _jumpPressed)
+        if (Physics.OverlapSphere(groundCheckTransform.position, 0.1f, playerMask).Length != 0 && _jumpPressed)
         {
             _rigidbody.AddForce(Vector3.up * JumpForce, ForceMode.VelocityChange);
             _jumpPressed = false;
         }
 
         var velocity = _rigidbody.velocity;
-        velocity = new Vector3(_horizontalInput * SidewayForce, velocity.y, velocity.z);
+        velocity = new Vector3(_horizontalInput * SidewaysForce, velocity.y, velocity.z);
         _rigidbody.velocity = velocity;
     }
 
